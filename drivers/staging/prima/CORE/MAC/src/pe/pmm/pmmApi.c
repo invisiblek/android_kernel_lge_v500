87e242396fa63fffb128feee578a7e252c3bcfae
SaidiReddy Yenuga <saidir@codeaurora.org>
2016-02-25 11:25:42 +0530

    wlan: Disconnect the session when WaitForKey timer expires
    Currently HDD is blocked as STA is in connected state and EAPOL
    exchange get timed out.
    After EAPOL exchange getting timed out driver should
    disconnect the session so that HDD is unblocked and can process
    subsequent scan requests.
    Fix this by issuing disconnect when WaitforKey timer expires.
    
    Change-Id: Ieff88735524d7572c037b22249dfae6471655059
    CRs-Fixed: 981619

--- drivers/staging/prima/CORE/MAC/src/pe/pmm/pmmApi.c
+++ drivers/staging/prima/CORE/MAC/src/pe/pmm/pmmApi.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2016 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -282,7 +282,7 @@ failure:
        {
            if(VOS_TRUE != tx_timer_running(&pMac->lim.limTimers.gLimHeartBeatTimer))
            {
-               PELOGE(pmmLog(pMac, LOGE, FL("Unexpected heartbeat timer not running"));)
+               PELOGE(pmmLog(pMac, LOGW, FL("Unexpected heartbeat timer not running"));)
                limReactivateHeartBeatTimer(pMac, psessionEntry);
            }
        }
@@ -473,7 +473,7 @@ void pmmInitBmpsPwrSave(tpAniSirGlobal pMac)
        {
            if(VOS_TRUE != tx_timer_running(&pMac->lim.limTimers.gLimHeartBeatTimer))
            {
-               PELOGE(pmmLog(pMac, LOGE, FL("Unexpected heartbeat timer not running"));)
+               PELOGE(pmmLog(pMac, LOGW, FL("Unexpected heartbeat timer not running"));)
                limReactivateHeartBeatTimer(pMac, psessionEntry);
            }
        }
